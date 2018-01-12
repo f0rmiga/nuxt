c3cbc8b1a6672300730c5a90bd2ea2c23c460532
Thulio Ferraz Assis <contato@thulioassis.com.br>
2018-01-11 21:22:56 -0800

    Added margin-top to the index card

--- template/pages/index.vue
+++ template/pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <v-layout column justify-center align-center>
     <v-flex xs12 sm8 md6>
-      <v-card>
+      <v-card class="card">
         <v-card-title class="headline">Welcome to the Vuetify + Nuxt.js template</v-card-title>
         <v-card-text>
           <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
@@ -21,3 +21,9 @@
     </v-flex>
   </v-layout>
 </template>
+
+<style lang="css" scoped>
+.card {
+  margin-top: 50px;
+}
+</style>
